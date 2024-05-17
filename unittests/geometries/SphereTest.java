dd72ee23218e46f8802dc36d76ac850b212095c6
chagit8155 <cradia@g.jct.ac.il>
2024-05-17 14:59:19 +0300

    Create tests for Sphere

--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -1,12 +1,20 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-
+import primitives.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SphereTest {
-
+    private final double DELTA = 0.000001;
     @Test
     void getNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        Point pointOnSphere = new Point(1, 1, 1);
+        Point center = new Point(0, 0, 0);
+        double radius = 1.0;
+        Sphere sphere = new Sphere(radius, center);
+        Vector normal = sphere.getNormal(pointOnSphere);
+     // Vector expectedNormal = new Vector(0.57735,0.57735,0.57735);
+        assertEquals(1, normal.length() ,DELTA,"Incorrect calculation of sphere's normal");
     }
 }
