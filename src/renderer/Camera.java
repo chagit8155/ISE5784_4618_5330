353ea1f6f5c8c2560af61b1bd8d436e15c68665b
chagit8155 <cradia@g.jct.ac.il>
2024-06-05 14:22:21 +0300

    Add to Camera two fields ImageWriter and rayTracer and the methods printGrid, writeToImage

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,14 +1,14 @@
 package renderer;
 
-import primitives.Point;
-import primitives.Ray;
-import primitives.Util;
-import primitives.Vector;
+import primitives.*;
 
 import java.util.MissingResourceException;
 
 import static primitives.Util.isZero;
 
+/**
+ * @author H & H
+ */
 public class Camera implements Cloneable {
     private Point location;
     private Vector vRight;
@@ -18,9 +18,14 @@ public class Camera implements Cloneable {
     private double height = 0d;
     private double width = 0d;
     private double distance = 0d;
+    //
+    private ImageWriter imageWriter;
+    private RayTracerBase rayTracer;
 
     private Camera() {
     }
+
+// ***************** Getters **********************
 
     /**
      * @return the height of the view plane
@@ -75,17 +80,134 @@ public class Camera implements Cloneable {
         return new Ray(location, vIJ);
     }
 
+
+    @Override
+    protected Camera clone() {
+        // Creates and returns a copy of this object.
+        //return a clone of this instance
+        try {
+            return (Camera) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // Should never happen
+        }
+    }
+
+    //=================== Builder ======================
+
     public static class Builder {
         private final Camera camera = new Camera();
 
-
-        public Builder setLocation(Point location) {
-            if (location == null) {
-                throw new IllegalArgumentException("location cannot be null");
+        /**
+         * Builds and returns the Camera instance.
+         *
+         * @return the constructed Camera instance
+         * @throws MissingResourceException if any required field is missing
+         */
+        public Camera build() {
+            final String missing = "Missing rendering data";
+            final String cameraStr = "Camera";
+            if (camera.location == null) {
+                throw new MissingResourceException(missing, cameraStr, "location");
             }
+            if (camera.vTo == null) {
+                throw new MissingResourceException(missing, cameraStr, "vTo");
+            }
+            if (camera.vUp == null) {
+                throw new MissingResourceException(missing, cameraStr, "vUp");
+            }
+            if (Util.alignZero(camera.width) <= 0) {
+                throw new MissingResourceException(missing, cameraStr, "width");
+            }
+            if (Util.alignZero(camera.height) <= 0) {
+                throw new MissingResourceException(missing, cameraStr, "height");
+            }
+            if (Util.alignZero(camera.distance) <= 0) {
+                throw new MissingResourceException(missing, cameraStr, "distance");
+            }
+            if (camera.imageWriter == null) {
+                throw new MissingResourceException(missing, cameraStr, "imageWriter");
+            }
+            if (camera.rayTracer == null) {
+                throw new MissingResourceException(missing, cameraStr, "rayTracer");
+            }
+            camera.vRight = (camera.vTo.crossProduct(camera.vUp)).normalize();
+            return (Camera) camera.clone();
+        }
+
+
+
+        /**
+         * Prints a grid on the image with the specified interval and color.
+         *
+         * @param interval the interval between grid lines
+         * @param color    the color of the grid lines
+         */
+        public Builder printGrid(int interval, Color color) {
+            for (int i = 0; i < camera.imageWriter.getNx(); i++) {
+                for (int j = 0; j < camera.imageWriter.getNy(); j++) {
+                    if (i % interval == 0 || j % interval == 0) {
+                        camera.imageWriter.writePixel(i, j, color); //?
+                    }
+                }
+            }
+            return this;
+        }
+
+        /**
+         * Writes the image to a file.
+         */
+        public void writeToImage() {
+            camera.imageWriter.writeToImage();
+        }
+
+
+        // ***************** Setters **********************
+
+        /**
+         * Sets the location of the camera.
+         *
+         * @param location the Point representing the camera's location. Must not be null.
+         * @return the current Builder instance for method chaining.
+         * @throws IllegalArgumentException if the location is null.
+         */
+        public Builder setLocation(Point location) {
+//            if (location == null) {
+//                throw new IllegalArgumentException("location cannot be null");
+//            }
             camera.location = location;
             return this;
         }
+
+        /**
+         * Sets the ImageWriter for the camera.
+         *
+         * @param imageWriter the ImageWriter instance. Must not be null.
+         * @return the current Builder instance for method chaining.
+         * @throws IllegalArgumentException if the imageWriter is null.
+         */
+        public Builder setImageWriter(ImageWriter imageWriter) {
+            if (imageWriter == null) {
+                throw new IllegalArgumentException("imageWriter cannot be null");
+            }
+            camera.imageWriter = imageWriter;
+            return this;
+        }
+
+        /**
+         * Sets the RayTracer for the camera.
+         *
+         * @param rayTracer the RayTracerBase instance. Must not be null.
+         * @return the current Builder instance for method chaining.
+         * @throws IllegalArgumentException if the rayTracer is null.
+         */
+        public Builder setRayTracer(RayTracerBase rayTracer) {
+            if (rayTracer == null) {
+                throw new IllegalArgumentException("rayTracer cannot be null");
+            }
+            camera.rayTracer = rayTracer;
+            return this;
+        }
+
 
         /**
          * Sets the direction vectors of the camera.
@@ -142,48 +264,7 @@ public class Camera implements Cloneable {
         }
 
 
-        /**
-         * Builds and returns the Camera instance.
-         *
-         * @return the constructed Camera instance
-         * @throws MissingResourceException if any required field is missing
-         */
-        public Camera build() {
-            ////try ct
-            final String missing = "Missing rendering data";
-            if (camera.location == null) {
-                throw new MissingResourceException(missing, "Camera", "location");
-            }
-            if (camera.vTo == null) {
-                throw new MissingResourceException(missing, "Camera", "vTo");
-            }
-            if (camera.vUp == null) {
-                throw new MissingResourceException(missing, "Camera", "vUp");
-            }
-            if (camera.width == 0) {
-                throw new MissingResourceException(missing, "Camera", "width");
-            }
-            if (camera.height == 0) {
-                throw new MissingResourceException(missing, "Camera", "height");
-            }
-            if (camera.distance == 0) {
-                throw new MissingResourceException(missing, "Camera", "distance");
-            }
-            return (Camera) camera.clone();
-        }
     }
 
-
-    @Override
-    protected Camera clone() {
-        //      Creates and returns a copy of this object.
-//    return a clone of this instance
-//
-        try {
-            return (Camera) super.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError(); // Should never happen
-        }
-    }
 
 }
