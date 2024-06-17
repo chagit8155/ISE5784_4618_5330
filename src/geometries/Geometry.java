8d3eac3670ab7d75e0a0555e15b434fa31326651
chagit8155 <cradia@g.jct.ac.il>
2024-06-18 01:52:31 +0300

    add a new field named material of type Material in the Geometry class

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -11,9 +11,25 @@ import java.util.List;
  */
 public abstract class Geometry extends Intersectable {
 
-// stage 6 - Because we added a field, we changed the interface to an abstract class
-// Field for emission color, initialized to black
-protected Color emission = Color.BLACK;
+    // stage 6 - Because we added a field, we changed the interface to an abstract class
+    // Field for emission color, initialized to black
+    protected Color emission = Color.BLACK;
+
+
+    // The material of the geometry
+    private Material material = new Material();
+
+
+
+    /**
+     * Returns the material of the geometry.
+     *
+     * @return the material of the geometry
+     */
+    public Material getMaterial() {
+        return material;
+    }
+
 
     /**
      * Getter for the emission color.
@@ -36,6 +52,16 @@ protected Color emission = Color.BLACK;
     }
 
 
+    /**
+     * Sets the material of the geometry.
+     *
+     * @param material the material to set
+     * @return the current Geometry object for chaining
+     */
+    public Geometry setMaterial(Material material) {
+        this.material = material;
+        return this;
+    }
 
     /**
      * Gets the normal vector (perpendicular) to the geometry at a given point.
@@ -44,8 +70,6 @@ protected Color emission = Color.BLACK;
      * @return the normal vector to the geometry at the given point
      */
     public abstract Vector getNormal(Point point);
-
-
 
 
     @Override
