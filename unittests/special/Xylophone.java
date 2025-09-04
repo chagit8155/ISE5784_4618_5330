ac6cd9ab948066956bc949a48968e9bf1acb6c51
chagit8155 <cradia@g.jct.ac.il>
2025-09-04 14:05:11 +0300

    upload the directory images

---
+++ unittests/special/Xylophone.java
@@ -0,0 +1,298 @@
+package special;
+import geometries.*;
+import lighting.*;
+import primitives.*;
+import renderer.*;
+import scene.Scene;
+import org.junit.jupiter.api.Test;
+import static java.awt.Color.*;
+
+public class Xylophone {
+
+    private Scene scene = new Scene("Test scene");
+
+
+    /**
+     * Image of Xylophone using  multithreading
+     */
+    @Test
+    public void Xylophone(){
+//        Camera camera = new Camera(new Point(-6000, -6000, 3200),
+//                new Vector(1, 1, -1d/2), new Vector(1, 1, 4)) //
+//                .setVPSize(200, 200).setVPDistance(1700);
+         final Camera.Builder camera =
+                Camera.getBuilder()
+                        .setDebugPrint(1)   //
+                        .setMultithreading(3)  //
+                        .setLocation(new Point(-6000, -6000, 3200))
+                        .setDirection(new Vector(1, 1, -1d/2),new Vector(1, 1, 4)) //
+                        .setVpDistance(1700)
+                        .setVpSize(200, 200) ;//
+
+
+
+
+
+        Scene scene = new Scene("Test scene")
+                .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.05)))
+                .setBackground(new Color(25,50,100));
+
+        scene.geometries.add( //
+
+                /**
+                 * Body
+                 */
+                new Polygon(
+                        new Point(-300,200,75),
+                        new Point(-300,-200,75),
+                        new Point(400,-140,75),
+                        new Point(400,140,75))//
+                        .setEmission(new Color(42, 80, 87)) //
+                        .setMaterial(new Material().setKr(0.4).setKs(0.1).setShininess(10)),//
+
+
+                new Polygon(
+                        new Point(-300,200,0),
+                        new Point(-300,-200,0),
+                        new Point(400,-140,0),
+                        new Point(400,140,0))//
+                        .setEmission(new Color(42, 115, 87)) //
+                        .setMaterial(new Material().setKr(0.4).setKs(0.1).setShininess(10)),
+
+
+
+                new Polygon(
+                        new Point(-300,200,0),
+                        new Point(-300,-200,0),
+                        new Point(-300,-200,75),
+                        new Point(-300,200,75))//
+                        .setEmission(new Color(42, 115, 87)) //
+                        .setMaterial(new Material().setKr(0.4).setKs(0.1).setShininess(10)),
+
+
+                new Polygon(
+                        new Point(400,-140,75),
+                        new Point(400,140,75),
+                        new Point(400,140,0),
+                        new Point(400,-140,0))//
+                        .setEmission(new Color(42, 115, 87)) //
+                        .setMaterial(new Material().setKr(0.4).setKs(0.1).setShininess(10)),
+
+
+
+
+
+                new Polygon(
+                        new Point(-300,-200,75),
+                        new Point(-300,-200,0),
+                        new Point(400,-140,0),
+                        new Point(400,-140,75))//
+                        .setEmission(new Color(42, 115, 87)) //
+                        .setMaterial(new Material().setKr(0.1).setKs(0.1).setShininess(10)),
+
+
+
+                new Polygon(
+                        new Point(-300,200,75),
+                        new Point(-300,200,0),
+                        new Point(400,140,0),
+                        new Point(400,140,75))//
+                        .setEmission(new Color(42, 115, 87)) //
+                        .setMaterial(new Material().setKr(0.1).setKs(0.1).setShininess(10)),
+
+
+
+                /**
+                 * Pads
+                 */
+
+
+
+                new Polygon(
+
+                        new Point(-260,180,75.2),
+                        new Point(-260,-180,75.2),
+                        new Point(-200,-180,75.2),
+                        new Point(-200,180,75.2))//
+                        .setEmission(new Color(RED)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+
+                new Polygon(
+
+                        new Point(-180,173,75.2),
+                        new Point(-180,-173,75.2),
+                        new Point(-120,-173 ,75.2),
+                        new Point(-120,173,75.2))//
+                        .setEmission(new Color(255,150,0)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+                new Polygon(
+
+                        new Point(-100,166,75.2),
+                        new Point(-100,-166,75.2),
+                        new Point(-40,-166 ,75.2),
+                        new Point(-40,166,75.2))//
+                        .setEmission(new Color(YELLOW)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+                new Polygon(
+
+                        new Point(-20,159,75.2),
+                        new Point(-20,-159,75.2),
+                        new Point(40,-159 ,75.2),
+                        new Point(40,159,75.2))//
+                        .setEmission(new Color(GREEN)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+                new Polygon(
+
+                        new Point(60,151,75.2),
+                        new Point(60,-151,75.2),
+                        new Point(120,-151 ,75.2),
+                        new Point(120,151,75.2))//
+                        .setEmission(new Color(CYAN)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+                new Polygon(
+
+                        new Point(140,144,75.2),
+                        new Point(140,-144,75.2),
+                        new Point(200,-144 ,75.2),
+                        new Point(200,144,75.2))//
+                        .setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+
+                new Polygon(
+
+                        new Point(220,137,75.2),
+                        new Point(220,-137,75.2),
+                        new Point(280,-137 ,75.2),
+                        new Point(280,137,75.2))//
+                        .setEmission(new Color(MAGENTA)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10).setGlossiness(5)),
+
+
+                new Polygon(
+
+                        new Point(300,130,75.2),
+                        new Point(300,-130,75.2),
+                        new Point(360,-130 ,75.2),
+                        new Point(360,130,75.2))//
+                        .setEmission(new Color(PINK)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10).setGlossiness(5)),
+
+
+                /**
+                 * floor
+                 */
+
+
+                new Polygon(
+                        new Point(-400,300,-10),
+                        new Point(-400,-330,-10),
+                        new Point(450,-330,-10),
+                        new Point(450,350,-10))//
+                        .setEmission(new Color(197, 80, 87).reduce(2)) //
+                        .setMaterial(new Material().setKr(0.1).setKs(0.1).setShininess(10).setGlossiness(5)),
+
+
+                new Polygon(
+                        new Point(-400,300,250),
+                        new Point(-400,300,-10),
+                        new Point(450,350,-10),
+                        new Point(450,350,250))//
+                        .setEmission(new Color(197, 80, 87).reduce(3)) //
+                        .setMaterial(new Material().setKr(0.1).setKs(0.1).setShininess(10).setGlossiness(5)),
+
+
+
+
+
+                new Cylinder(15d,new Ray(new Point(-350,280,-10),new Vector(0, 0, -1)),  120).setEmission(new Color(53, 47, 70))// Foot
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30).setGlossiness(5)),
+                new Cylinder(15d,new Ray( new Point(-350,-306,-10),new Vector(0, 0, -1)),  120).setEmission(new Color(53, 47, 70))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30).setGlossiness(5)),
+                new Cylinder(15d,new Ray(new Point(410,-306,-10),new Vector(0, 0, -1)),  120).setEmission(new Color(53, 47, 70))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30).setGlossiness(5)),
+                new Cylinder(15d,new Ray(new Point(410,280,-10),new Vector(0, 0, -1)),  120).setEmission(new Color(53, 47, 70))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30).setGlossiness(5)),
+
+
+
+                new Cylinder(7d,new Ray(new Point(-100, 123, 100),new Vector(1, 0, 0) ),  185).setEmission(new Color(BLACK))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30)),
+                new Cylinder(7d,new Ray(new Point(-180, -77, 100),new Vector(1, 0, 0)),  180).setEmission(new Color(BLACK))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30)),
+
+                new Sphere(26d,new Point(-100, 123, 100)).setEmission(new Color(RED)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.2)),//
+                new Sphere(26d,new Point(-180, -77, 100)).setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.3)),
+
+
+
+
+
+
+
+                new Cylinder(7d,new Ray(new Point(350, -200, 10),new Vector(-0.77, -2.53, 0)),  185).setEmission(new Color(BLACK))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30)),
+                new Cylinder(7d,new Ray(new Point(260,-220,10),new Vector(-0.51, -2.39, 0)),  185).setEmission(new Color(BLACK))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30)),
+                new Cylinder(7d,new Ray(new Point(150, -240, 10),new Vector(-0.5, -2.33, 0)),  185).setEmission(new Color(BLACK))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30)),
+                new Cylinder(7d,new Ray(new Point(40,-250,10),new Vector(0.23, -2.73, 0)),  185).setEmission(new Color(BLACK))//
+                        .setMaterial(new Material().setKd(0.4).setKs(0.25).setShininess(30)),
+
+                new Sphere(20d,new Point(350, -200, 10)).setEmission(new Color(GREEN).reduce(3)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.2)),//
+                new Sphere(20,new Point(260, -220, 10)).setEmission(new Color(YELLOW).reduce(3)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.2)),//
+                new Sphere( 20,new Point(150, -240, 10)).setEmission(new Color(WHITE).reduce(3)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(1).setKt(0.01)),//
+                new Sphere( 20,new Point(40, -250, 10)).setEmission(new Color(2, 9, 197)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.2))//
+
+
+        );
+
+        scene.lights.add(new DirectionalLight(new Color(1000,800,400),new Vector(891,-828,-401)));
+
+        scene.lights.add(new DirectionalLight(new Color(800,600,300),new Vector(-600,400,-300)));
+
+        scene.lights.add(new PointLight(new Color(700, 400, 400), new Point(-550, -300, 500)).setKL(4E-5).setKQ(0.0000006));
+        scene.lights.add(new SpotLight(new Color(700, 400, 400),
+                new Point(-0, -0, -5000), new Vector(0, 0, 1)) //
+                .setKL(4E-5).setKQ(2E-7));
+        scene.lights.add(new SpotLight( new Color(800, 500, 0),
+                new Point(-50, -50, 25), new Vector(1, 1, -0.5))
+                .setNarrowBeam(10).setKL(0.001).setKQ(0.00004));
+        scene.lights.add(new SpotLight( new Color(800, 500, 400),
+                new Point( 0, 0, 500), new Vector(0, 0, -1))
+                .setNarrowBeam(30).setKL(0.001).setKQ(0.00004));
+
+
+
+        ImageWriter imageWriter = new ImageWriter("Xylophone", 600, 600);
+
+        camera.setImageWriter(imageWriter);
+        camera
+
+                .setRayTracer(new SimpleRayTracer(scene))
+                .build()
+                .renderImage()
+                .writeToImage();
+
+
+
+    }
+
+}
